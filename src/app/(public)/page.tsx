9578a834a95981f1df603f2d4c73043582934f1a
mark <mark.sva@hotmail.com>
2026-03-10 12:25:10 -0300

    footer component

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "./_components/footer";
 import { Header } from "./_components/header";
 import { Hero } from "./_components/hero";
 import { Professionals } from "./_components/professionals";
@@ -13,6 +14,8 @@ export default function Home() {
         <Hero />
         <Professionals />
       </div>
+
+      <Footer />
     </div>
   )
 }

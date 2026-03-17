a8ebe7ac777d6a8d803c1f47cfafac0072d1da48
mark <mark.sva@hotmail.com>
2026-03-17 15:16:47 -0300

    feat: implement user data retrieval in profile component

--- src/app/(panel)/dashboard/profile/page.tsx
+++ src/app/(panel)/dashboard/profile/page.tsx
@@ -1,5 +1,22 @@
-export default function Profile() {
-    return(
+import getSession from "@/lib/getSession";
+import { redirect } from "next/navigation";
+import { getUserData } from "./_data-access/get-info-user";
+
+export default async function Profile() {
+
+    const session = await getSession();
+
+    if (!session) {
+        redirect("/")
+    }
+
+    const user = await getUserData({ userId: session.user.id });
+
+    if (!user) {
+        redirect("/")
+    }
+
+    return (
         <section>
             <h1>profile</h1>
         </section>

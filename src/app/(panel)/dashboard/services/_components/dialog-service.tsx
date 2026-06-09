a7a93febf3f7448e52c1df5cb207fbbea23b1f69
mark <mark.sva@hotmail.com>
2026-06-08 23:55:37 -0300

    feat: implement service update functionality with form validation and error handling

--- src/app/(panel)/dashboard/services/_components/dialog-service.tsx
+++ src/app/(panel)/dashboard/services/_components/dialog-service.tsx
@@ -14,16 +14,24 @@ import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { convertRealToCents } from '@/utils/convertCurrency'
 import { createNewService } from '../_actions/create-service'
+import { updateService } from '../_actions/update-service'
 import { toast } from "sonner"
 import { useRouter } from 'next/navigation'
 
 interface DialogServiceProps {
   closeModal: () => void;
+  serviceId?: string;
+  initialValues?: {
+    name: string;
+    price: string;
+    hours: string;
+    minutes: string;
+  }
 }
 
-export function DialogService({ closeModal }: DialogServiceProps) {
+export function DialogService({ closeModal, initialValues, serviceId }: DialogServiceProps) {
 
-  const form = useDialogServiceForm()
+  const form = useDialogServiceForm({ initialValues: initialValues })
   const [loading, setLoading] = useState(false);
   const router = useRouter();
 
@@ -36,6 +44,17 @@ export function DialogService({ closeModal }: DialogServiceProps) {
 
     // Converter as horas e minutos para duração total em minutos;
     const duration = (hours * 60) + minutes;
+
+    if (serviceId) {
+      await editServiceById({
+        serviceId: serviceId,
+        name: values.name,
+        priceInCents: priceInCents,
+        duration: duration
+      })
+
+      return;
+    }
 
     const response = await createNewService({
       name: values.name,
@@ -54,6 +73,38 @@ export function DialogService({ closeModal }: DialogServiceProps) {
     toast.success("Serviço cadastrado com sucesso")
     handleCloseModal();
     router.refresh();
+
+  }
+
+
+  async function editServiceById({
+    serviceId,
+    name,
+    priceInCents,
+    duration }: {
+      serviceId: string,
+      name: string,
+      priceInCents: number,
+      duration: number
+    }) {
+
+    const response = await updateService({
+      serviceId: serviceId,
+      name: name,
+      price: priceInCents,
+      duration: duration
+    })
+
+    setLoading(false);
+
+
+    if (response.error) {
+      toast(response.error)
+      return;
+    }
+
+    toast(response.data)
+    handleCloseModal();
 
   }
 
@@ -187,7 +238,7 @@ export function DialogService({ closeModal }: DialogServiceProps) {
             className="w-full font-semibold text-white"
             disabled={loading}
           >
-            {loading ? "Cadastrando..." : "Adicionar serviço"}
+            {loading ? "Carregando..." : `${serviceId ? "Atualizar serviço" : "Cadastrar serviço"}`}
           </Button>
 
         </form>
